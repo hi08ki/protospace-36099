50937152fe8afd6c36648e3166723fb2c83a9896
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-19 17:20:21 +0900

    変更4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
+  #before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  #before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   def index
     @users = User.all 
@@ -24,17 +26,22 @@ class PrototypesController < ApplicationController
   def show
    @prototype = Prototype.find(params[:id])
    @comment = Comment.new
-   @comments = @prototype.comments
+   @comments = @prototype.comments.includes(:user)
    #@comments = Comment.find_by(id:params[:id], prototype_id: params[:prototype_id])     
   end
 
   def edit
     @prototype = Prototype.find(params[:id])
+  #  if @prototype.user_id == current_user.id
+  #     render :edit
+  #   else
+  #     redirect_to root_path 
+  #   end 
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
@@ -42,8 +49,8 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    if prototype.destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
       redirect_to root_path
     end    
   end
@@ -53,4 +60,7 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id:current_user.id)
   end
 
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @prototypes = current_user.prototypes
+    @prototypes = user.prototypes
+  end
+end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+     redirect_to users_path
+    else
+      render action: :new
   end
 end
