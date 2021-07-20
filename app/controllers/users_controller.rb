b1156f13207c6328629c1a084205e172787f65c6
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-20 12:07:39 +0900

    変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+  #before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @prototypes = current_user.prototypes
-    @prototypes = @user.prototypes
+    # @prototypes = current_user.prototypes
+    # @prototypes = @user.prototypes
   end
 end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-     redirect_to users_path
-    else
-      render action: :new
-  end
-end
+#   def create
+#     @user = User.new(user_params)
+#     if @user.save
+#      redirect_to users_path
+#     else
+#       render action: :new
+#   end
+# end
